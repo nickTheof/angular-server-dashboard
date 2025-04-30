51a833ea1330d2e7315090ad6d57cc4284f2bb69
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-04-30 13:29:17 +0300

    feat: add OnInit implementation for ItemServerStatusComponent

--- src/app/components/item-server-status/item-server-status.component.ts
+++ src/app/components/item-server-status/item-server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-item-server-status',
@@ -6,6 +6,21 @@ import { Component, signal } from '@angular/core';
   templateUrl: './item-server-status.component.html',
   styleUrl: './item-server-status.component.css',
 })
-export class ItemServerStatusComponent {
-  currentStatus = signal<string>('online');
+export class ItemServerStatusComponent implements OnInit {
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('online');
+
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+      const random = Math.random();
+      if (random < 0.5) {
+        this.currentStatus.set('online');
+      } else if (random < 0.9) {
+        this.currentStatus.set('offline');
+      } else {
+        this.currentStatus.set('unknown');
+      }
+    }, 5000);
+  }
 }
